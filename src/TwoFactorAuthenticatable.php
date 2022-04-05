d943b571dea33728c18f6f2f7ef03f54cc7efeab
Danilo Andrade <mandrade.danilo@gmail.com>
2022-04-05 20:50:06 -0300

    fix: add border to qrcode to fix scan in dark mode

--- src/TwoFactorAuthenticatable.php
+++ src/TwoFactorAuthenticatable.php
@@ -58,7 +58,7 @@ trait TwoFactorAuthenticatable
     {
         $svg = (new Writer(
             new ImageRenderer(
-                new RendererStyle(192, 0, null, null, Fill::uniformColor(new Rgb(255, 255, 255), new Rgb(45, 55, 72))),
+                new RendererStyle(192, 1, null, null, Fill::uniformColor(new Rgb(255, 255, 255), new Rgb(45, 55, 72))),
                 new SvgImageBackEnd
             )
         ))->writeString($this->twoFactorQrCodeUrl());
